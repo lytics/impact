4477ab58071d3a803c3829b77d940fe5c866b679
drewlanenga <drew.lanenga@gmail.com>
2015-05-04 16:19:03 -0700

    parallelize impact detection

--- detect_test.go
+++ detect_test.go
@@ -11,28 +11,6 @@ var (
 	mockShort = []float64{0.2, 0, 0.4, 0, 0.1, 0.5, 0.2, 0.4, 0, 0, 0.1, 0.6, 0.1, 0.3, 0.1, 0.1, 0.2, 0.3, 0.1, 0.1}
 )
 
-func TestSmooth(t *testing.T) {
-	smoothed := smooth(mockShort)
-
-	// check the first couple of smoothed values
-	assert.Equalf(t, true, equal(0.2, smoothed[0]), "incorrect smoothed value")
-	assert.Equalf(t, true, equal(0.15, smoothed[1]), "incorrect smoothed value")
-	assert.Equalf(t, true, equal(0.14, smoothed[2]), "incorrect smoothed value")
-	assert.Equalf(t, true, equal(0.2, smoothed[3]), "incorrect smoothed value")
-	assert.Equalf(t, true, equal(0.24, smoothed[4]), "incorrect smoothed value")
-}
-
-func TestDiff(t *testing.T) {
-	mockDiff := diff(mockShort)
-
-	// make sure the difference is the right length
-	assert.Equal(t, len(mockShort)-1, len(mockDiff))
-
-	// make sure the first couple differences are correct
-	assert.Equalf(t, true, equal(-0.2, mockDiff[0]), "incorrect vector difference")
-	assert.Equalf(t, true, equal(0.4, mockDiff[1]), "incorrect vector difference")
-}
-
 func TestComparisons(t *testing.T) {
 	greaterCount := gt(0.3, mockShort)
 	assert.Equal(t, 4, greaterCount)
@@ -41,21 +19,11 @@ func TestComparisons(t *testing.T) {
 	assert.Equal(t, 14, lessCount)
 }
 
-func TestWalk(t *testing.T) {
-	n := 10
-	randomWalk := walk(10.0, n, mockShort)
-
-	assert.Equal(t, n, len(randomWalk))
-
-	randomMean := mean(randomWalk)
-	assert.Tf(t, randomMean > 7.0, "mean of random walk is too low")
-	assert.Tf(t, randomMean < 13.0, "mean of random walk is too high")
-}
-
 func TestDetect(t *testing.T) {
 	// get the pvalue and operator for the test
-	p, op := DetectImpact(mockShort[0:14], mockShort[14:20], 200)
-	assert.Tf(t, p < 0.1, "pvalue for Detect should be small (likely < 0.05)")
+	p, op, err := DetectImpact(mockShort[0:14], mockShort[14:20], 1000)
+	assert.Equal(t, nil, err)
+	assert.Tf(t, p > 0.1 && p < 0.25, "pvalue for Detect should be within [0.1, 0.2]")
 	assert.Equalf(t, LESS_THAN, op, "the series detection should show a decrease")
 }
 
